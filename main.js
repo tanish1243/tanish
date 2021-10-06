3b43cebabc537129067de5bcd8f7f3f81ea8323c
tanish1243 <71543868+tanish1243@users.noreply.github.com>
2021-10-06 17:43:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+leftwristx="";
+rightwristx="";
+distance="";
+nosex="";
+nosey="";
+
+
 function setup(){
-canvas = createCanvas(550 ,500);
+ canvas = createCanvas(550 ,500);
 canvas.position(785, 150)
- 
+
 video= createCapture(VIDEO);
 video.size(550 ,550);
 
@@ -14,7 +21,15 @@ function modelloaded(){
 function gotPoses(Results){
     if(Results.length > 0){
         console.log(Results)
+     leftwristx= Results[0].pose.leftWrist.x;
+     rightwristx= Results[0].pose.rightWrist.x;
+     distance= floor(leftwristx-rightwristx)
+     nosex= Results[0].pose.nose.x;
+     nosey= Results[0].pose.nose.y;
+
+
     }
+
 }
 
 
@@ -22,4 +37,8 @@ function gotPoses(Results){
 
  function draw(){
 background("#42b0d4") 
+textSize(distance);
+fill("red");
+text("Tanish",nosex,nosey)
+document.getElementById("fontsize").innerHTML="Font Size Of Text Is " + distance + " pixels"
  }
